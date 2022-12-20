3d20e376d5f9027240da12e4e01fa8b3400d4188
Annie <annie.gallagher7@gmail.com>
2022-12-20 11:51:18 -0700

    adds event handling for task completion and maps task list to task

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
-
 const App = () => {
+  const taskDataList = [
+    {
+      id: 1,
+      title: 'Mow the lawn',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Cook Pasta',
+      isComplete: true,
+    },
+  ];
+
+  const [taskData, setTaskData] = useState(taskDataList);
+
+  const completeTask = (id) => {
+    setTaskData((currentTaskData) =>
+      currentTaskData.map((task) => {
+        if (task.id === id) {
+          return { ...task, isComplete: !task.isComplete };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          <TaskList tasks={taskData} onUpdateTask={completeTask}></TaskList>
+        </div>
       </main>
     </div>
   );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,20 +3,22 @@ import PropTypes from 'prop-types';
 import Task from './Task';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
-  const getTaskListJSX = (tasks) => {
-    return tasks.map((task) => {
-      return (
-        <Task
-          key={task.id}
-          id={task.id}
-          title={task.title}
-          isComplete={task.isComplete}
-        />
-      );
-    });
-  };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+const TaskList = ({ tasks, onUpdateTask }) => {
+  return (
+    <ul className="tasks__list no-bullet">
+      {tasks.map((task) => {
+        return (
+          <Task
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            isComplete={task.isComplete}
+            onUpdate={onUpdateTask}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 TaskList.propTypes = {
@@ -27,6 +29,7 @@ TaskList.propTypes = {
       isComplete: PropTypes.bool.isRequired,
     })
   ).isRequired,
+  onUpdateTask: PropTypes.func.isRequired,
 };
 
 export default TaskList;
